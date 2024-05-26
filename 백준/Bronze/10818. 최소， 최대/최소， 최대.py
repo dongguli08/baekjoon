3a609b0370eda1e6fa468e2311fb2d2a82aeba0a
HADONGGUN <163298112+dongguli08@users.noreply.github.com>
2024-05-26 20:59:16 +0900

    [Bronze III] Title: 최소, 최대, Time: 420 ms, Memory: 152816 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -3,9 +3,9 @@ a_list=list(map(int,input().split()))
 max=a_list[0]
 min=a_list[0]
 for i in a_list:
-   if i>max:
-      max=i 
-   elif i<min:
-      min=i
+     if i>max:
+          max=i
+     elif i<min:
+          min=i
 
 print(min,max)
