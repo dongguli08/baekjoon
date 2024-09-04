e94bd02c6bc5875618fbf8b18df1377fdbc5fdff
하동건 <163298112+dongguli08@users.noreply.github.com>
2024-09-04 17:11:34 +0900

    [level 0] Title: 카운트 업, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.py
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.py
@@ -1,2 +1,5 @@
 def solution(start, end):
-    return tuple(range(start, end + 1))
+    answer = []  # 빈 리스트로 초기화
+    for i in range(start, end + 1):
+        answer.append(i)  # 리스트에 i 추가
+    return answer  # 루프가 끝난 후 리스트 반환
