5d45868e522cf75c3ea0a51731fd1824d25a943b
하동건 <163298112+dongguli08@users.noreply.github.com>
2024-09-04 16:53:01 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/README.md
+++ 프로그래머스/0/181930. 주사위 게임 2/README.md
@@ -1,10 +1,10 @@
 # [level 0] 주사위 게임 2 - 181930 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181930?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181930) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.02 ms
+메모리: 10.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 04일 14:22:54
+2024년 09월 04일 16:52:59
 
 ### 문제 설명
 
--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -1,13 +1,8 @@
-import math
-
 def solution(a, b, c):
-    answer = 0
-    
-    if a == b and b == c and a == c:
-        answer = (a + b + c) * ((math.pow(a, 2)) + (math.pow(b, 2)) + (math.pow(c, 2))) * ((math.pow(a, 3)) + (math.pow(b, 3)) + (math.pow(c, 3)))
-    elif a == b or b == c or a == c:
-        answer = (a + b + c) * ((math.pow(a, 2)) + (math.pow(b, 2)) + (math.pow(c, 2)))
+    if a==b and b==c:
+        return (a+b+c) *(a**2 + b**2 + c**2)*(a**3+b**3+c**3)
+    elif a!=b and b!=c and a!=c:
+        return a+b+c
     else:
-        answer = (a + b + c)
+        return (a+b+c)*(a**2+b**2+c**2)
     
-    return answer
