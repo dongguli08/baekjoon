f0b0c45537aabf2151270deae0c187d5ff833d88
하동건 <163298112+dongguli08@users.noreply.github.com>
2024-09-13 11:59:46 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -1,10 +1,25 @@
+// function solution(my_string, is_prefix) {
+//     var answer = 0;
+//      if (my_string.startsWith(is_prefix)){
+//          answer = 1
+//      }
+//     else{
+//         answer = 0
+//     }
+//     return answer;
+// }
 function solution(my_string, is_prefix) {
     var answer = 0;
-     if (my_string.startsWith(is_prefix)){
-         answer = 1
-     }
-    else{
-        answer = 0
+
+    if(my_string.length < is_prefix.length){
+        return 0;
     }
-    return answer;
+
+    for(let i = 0; i<is_prefix.length; i++){
+        if(my_string[i] !== is_prefix[i]){
+            return 0;
+        }
+    }
+
+    return 1;;
 }
