3a9bd129d637e64091e00ed781636da9ef9c88de
하동건 <163298112+dongguli08@users.noreply.github.com>
2024-09-21 18:27:41 +0900

    [level 0] Title: 카운트 다운, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.js
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.js
@@ -1,6 +1,6 @@
 function solution(start_num, end_num) {
     var answer = [];
-    for (i=start_num; i>=end_num; i--){
+    for(let i=start_num; i>=end_num; i--){
         answer.push(i)
     }
     return answer;
