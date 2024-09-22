04875a7f5881f31ec2b4f54dbdae70d3841e324c
하동건 <163298112+dongguli08@users.noreply.github.com>
2024-09-22 10:39:42 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -5,11 +5,10 @@ def solution(num_list):
     
     if a > b:
         c = a - b
-        # 정수를 리스트로 변환 후 추가
         return num_list + [c]
     else:
         c = a * 2
-        # 정수를 리스트로 변환 후 추가
+        
         return num_list + [c]
 #list(c)에서 c가 정수형이면 TypeError가 발생합니다. 이는 list 함수가 반복 가능한 객체만을 인자로 받아야 하기 때문입니다. 
 # 즉 반복 가능한 객체는 튜플,문자열 등
