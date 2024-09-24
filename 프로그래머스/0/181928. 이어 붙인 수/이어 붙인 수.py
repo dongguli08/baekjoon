4931b0406b84891cf260177b8f7471486f071a7d
하동건 <163298112+dongguli08@users.noreply.github.com>
2024-09-24 16:22:20 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
@@ -1,10 +1,20 @@
 def solution(num_list):
     answer1 = ''
     answer2 = ''
-    
-    for i in num_list:
-        if i%2==0:
-            answer1 += str(i)
-        elif i%2!=0:
-            answer2+= str(i)
+        
+    for i in range(len(num_list)):
+        if num_list[i]%2==0:
+            answer1 += str(num_list[i])
+        elif num_list[i]%2!=0:
+            answer2 += str(num_list[i])
     return int(answer1)+int(answer2)
+    
+    
+    # for i in num_list:
+    #     if i%2==0:
+    #         answer1 += str(i)
+    #     elif i%2!=0:
+    #         answer2+= str(i)
+    # return int(answer1)+int(answer2)
+
+
