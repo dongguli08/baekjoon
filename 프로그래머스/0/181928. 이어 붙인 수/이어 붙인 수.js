b9dc0fffe456bd503b9ecaef244eedd0644ff873
하동건 <163298112+dongguli08@users.noreply.github.com>
2024-09-24 16:48:55 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
@@ -2,15 +2,14 @@ function solution(num_list) {
     var answer = 0;
     let num1=''
     let num2 = ''
-    
-    for(let i =0; i<num_list.length; i++){
+  for (let i = 0; i<num_list.length; i++){
         if(num_list[i]%2==0){
-            num1+=String(num_list[i])
+            num1+=num_list[i]
         }
-        else if(num_list[i]%2!=0){
-            num2+=String(num_list[i])
-        }
-    }
-    
-    return parseInt(num1)+Number(num2);
+      else{
+          num2+=num_list[i]
+      }
+  }
+    return Number(num1) + parseInt(num2)
 }
+
