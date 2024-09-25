26596859b0ea223802d5f55ec508206139aa591b
하동건 <163298112+dongguli08@users.noreply.github.com>
2024-09-25 13:04:16 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,10 +1,13 @@
-function solution(num_list) {
-    let last =num_list[num_list.length-1] 
-    let last2 = num_list[num_list.length-2]
-    if(last > last2) {
-       num_list.push(last-last2)
+    function solution(num_list) {
+    var answer = [];
+    let last1 = num_list[num_list.length - 1];  // 배열의 마지막 요소를 참조
+    let last2 = num_list[num_list.length - 2];  // 배열의 두 번째 마지막 요소를 참조
+    
+    if (last1 > last2) {
+        num_list.push(last1 - last2);  // 마지막 요소가 두 번째 마지막 요소보다 크면 차이를 추가
     } else {
-        num_list.push(last*2)
-    }
-  return num_list
+        num_list.push(last1 * 2);  // 그렇지 않으면 마지막 요소의 두 배를 추가
+    }  
+    
+    return num_list;
 }
