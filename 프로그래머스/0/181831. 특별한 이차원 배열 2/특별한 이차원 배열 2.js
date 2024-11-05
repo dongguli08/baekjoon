eb8fa7d67ae3d8ee0bff58867682698d833814d6
하동건 <163298112+dongguli08@users.noreply.github.com>
2024-11-05 15:37:13 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 1.95 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -11,7 +11,3 @@ function solution(arr) {
     }
     return answer;
 }
-
-console.log(solution([[5, 192, 33], [192, 72, 95], [33, 95, 999]]))
-console.log(solution([[19, 498, 258, 587], [63, 93, 7, 754], [258, 7, 1000, 723],
- [587, 754, 723, 81]]))
