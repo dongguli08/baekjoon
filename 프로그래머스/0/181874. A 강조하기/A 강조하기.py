90feda360df709fe8ae566b9d5c0eb9a6f82cdfb
하동건 <163298112+dongguli08@users.noreply.github.com>
2025-10-12 20:27:42 +0900

    [level 0] Title: A 강조하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.py
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.py
@@ -1,8 +1,4 @@
 def solution(myString):
     answer = ''
-    for i in myString:
-        if i.lower()!= 'a':
-            answer +=i.lower()
-        else:
-            answer += 'A'
-    return answer
+    answer = myString.lower()
+    return answer.replace('a', 'A')
