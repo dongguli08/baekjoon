437d94b0990746dbd797cdafc9246af55089ec25
하동건 <163298112+dongguli08@users.noreply.github.com>
2025-12-12 09:23:03 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,21 +1,28 @@
 def solution(n, lost, reserve):
+    answer = 0
+    # 1.set으로 자료형을 바꾸고
+    # 2.lost와 reserve에 같은 값이 있으면 둘 다에서 삭제
+    # 3.for 문을 돌면서 r-1 값이 lost에 있는지
+    # 4. r+1값이 lost에 있는지 확인
+    # 5. n에서 lost의 길이 만큼 빼기
+    
     lost.sort()
     reserve.sort()
-
-    lost_set = set(lost)
+    
+    #set 으로 바꾸는 방법 알기 (list->set은 중복제거와,인덱스로 접근 불가)
     reserve_set = set(reserve)
+    lost_set = set(lost)
 
-    # lost와 reserve 중복 제거 (자기 자신만 해결하는 경우)
-    overlap = lost_set & reserve_set
-    lost_set -= overlap
-    reserve_set -= overlap
-
-    # 빌려주는 처리
+    both = reserve_set & lost_set
+    reserve_set -=both
+    lost_set -= both
+    
     for r in reserve_set:
-        if r - 1 in lost_set:
-            lost_set.remove(r - 1)
-        elif r + 1 in lost_set:
-            lost_set.remove(r + 1)
-
-    answer = n - len(lost_set)
+        if r-1 in lost_set:
+            lost_set.remove(r-1) # lost_set에서 r-1 값 삭제
+        elif r+1 in lost_set:
+            lost_set.remove(r+1)
+            
+    answer = n-len(lost_set)
+    
     return answer
