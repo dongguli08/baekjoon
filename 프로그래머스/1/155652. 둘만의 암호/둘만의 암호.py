828b9edaf5ed0c9a8f70f75d8f1bdbf58ec871a1
하동건 <163298112+dongguli08@users.noreply.github.com>
2025-12-15 08:44:09 +0900

    [level 1] Title: 둘만의 암호, Time: 0.21 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
@@ -5,18 +5,18 @@ def solution(s, skip, index):
     skip_set = set(skip)      # 포함 여부 O(1)
     answer = ''
 
-    for ch in s:
-        cur = alphabet.index(ch)
-        moved = 0
-
-        while moved < index:
-            cur = (cur + 1) % 26   # z 넘어가면 a로 순환
-
-            if alphabet[cur] in skip_set:
-                continue          # skip이면 카운트 안 함
-
-            moved += 1
-
-        answer += alphabet[cur]
-
+    for i in s:
+        location = alphabet.index(i) #현재 알파벳 인덱스 위치
+        number = 0
+        
+        while number < index:
+            location = (location+1)%26
+        
+            if alphabet[location] in skip_set: 
+                continue
+                
+            number +=1
+        
+        answer+=alphabet[location] #현재 인덱스의 알파벳
+        
     return answer
