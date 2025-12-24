00c4e0527ad8865a55003a3a86d29792547d9140
하동건 <163298112+dongguli08@users.noreply.github.com>
2025-12-24 20:19:25 +0900

    [level 1] Title: 삼총사, Time: 0.05 ms, Memory: 9.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -0,0 +1,8 @@
+from itertools import combinations
+
+def solution(number):
+    answer = 0
+    for nums in combinations(number,3):
+        if sum(nums) == 0:
+            answer+=1
+    return answer
