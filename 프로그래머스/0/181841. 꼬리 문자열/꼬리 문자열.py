9f36c96a7a9410f9d2dd8eff430e84be4228f8dc
하동건 <163298112+dongguli08@users.noreply.github.com>
2026-01-27 23:26:48 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.py
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.py
@@ -3,4 +3,9 @@ def solution(str_list, ex):
     for i in str_list:
         if ex not in i:
             answer += i
-    return answer
+    return answer 
+#인덱스 번호는 필요가 없음 어짜피 결과는 문자열로 만들거임
+# i 가 단어 묶음 하나씩 순환을 함 "abc"가 인덱스 1개 
+#만약 abc에 ex가 포함된다면 넘어가고 포함되지 않으면 answer에 문자형태로 추가
+
+
